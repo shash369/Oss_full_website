04267701cd8049d461ed1050a29ac1438266e155
shashwat shukla <shuklashashwat009@gmail.com>
2024-09-15 15:08:34 +0530

    al-update

--- src/app/alumni/page.js
+++ src/app/alumni/page.js
@@ -3,7 +3,7 @@ export default function AlumniD(){
     return (
         <div className="min-h-screen pb-32 overflow-hidden sm:grid sm:justify-items-center sm:self-center mb-12">
         <div className=" pr-12 pb-12">
-            <h1 className="text-center pt-9 font-bold text-6xl  text-lime-400 pb-6">Fourth Year</h1>
+            <h1 className="text-center pt-9 font-bold text-6xl  text-lime-400 pb-6">2016-2020</h1>
             <div className="text-black grid grid-cols-1 gap-14 h-full w-full pt-6 pl-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
                 <MemBox name="Shashwat" role="Web Developer" img="/ml.jpg" baImg="/ml.jpg" linkedin="https://www.linkedin.com/in/shashwat" insta="https://www.instagram.com/shashwat" />
                 <MemBox name="Shashwat" role="Web Developer" img="/ml.jpg" baImg="/ml.jpg" linkedin="https://www.linkedin.com/in/shashwat" insta="https://www.instagram.com/shashwat" />
@@ -19,7 +19,7 @@ export default function AlumniD(){
         </div>
 
         <div className=" pr-12 pb-12">
-            <h1 className="text-center pt-20 font-bold text-6xl  text-lime-400 pb-6">Third Year</h1>
+            <h1 className="text-center pt-20 font-bold text-6xl  text-lime-400 pb-6">2015-2019</h1>
             <div className="text-black grid grid-cols-1 gap-14 h-full w-full pt-6 pl-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
                 <MemBox name="Shashwat" role="Web Developer" img="/ml.jpg" baImg="/ml.jpg" linkedin="https://www.linkedin.com/in/shashwat" insta="https://www.instagram.com/shashwat" />
                 <MemBox name="Shashwat" role="Web Developer" img="/ml.jpg" baImg="/ml.jpg" linkedin="https://www.linkedin.com/in/shashwat" insta="https://www.instagram.com/shashwat" />
@@ -35,7 +35,7 @@ export default function AlumniD(){
         </div>
 
         <div className=" pr-12 pb-12">
-            <h1 className="text-center pt-28 font-bold text-6xl  text-lime-400 pb-6">Second Year</h1>
+            <h1 className="text-center pt-28 font-bold text-6xl  text-lime-400 pb-6">2014-2018</h1>
             <div className="text-black grid grid-cols-1 gap-14 h-full w-full pt-6 pl-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
                 <MemBox name="Shashwat" role="Web Developer" img="/ml.jpg" baImg="/ml.jpg" linkedin="https://www.linkedin.com/in/shashwat" insta="https://www.instagram.com/shashwat" />
                 <MemBox name="Shashwat" role="Web Developer" img="/ml.jpg" baImg="/ml.jpg" linkedin="https://www.linkedin.com/in/shashwat" insta="https://www.instagram.com/shashwat" />
